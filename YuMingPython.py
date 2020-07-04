707d603d9e0521e095e54e7af15efd4c46511973
YuMing1234 <63939718+YuMing1234@users.noreply.github.com>
2020-07-04 09:33:28 +0800

    Update YuMingPython.py

--- YuMingPython.py
+++ YuMingPython.py
@@ -5,9 +5,15 @@ Created on Sat Jun 27 14:18:26 2020
 @author: USER1
 """
 
-route = [500,200,700]
+filein = open("salein.txt","r")
+route = []
+for i in range(3):
+    content = filein.readline()
+    route.append(int(content))
+filein.close()
 go = max(route)
 route.remove(max(route))
 back = max(route)
 peoplevisited = go + back
-print(peoplevisited)
+fileout = open("saleout.txt","w")
+fileout.write(str(peoplevisited))
